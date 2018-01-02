3653c031f573f29b7172b89de987f5f075fe5dec
slowdony <435574793@qq.com>
2018-01-03 00:03:49 +0800

    初始化

---
+++ SDInKe/Class/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDInKe
+//
+//  Created by slowdony on 2018/1/2.
+//  Copyright © 2018年 slowdony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDInKe/Class/Other/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDInKe
+//
+//  Created by slowdony on 2018/1/2.
+//  Copyright © 2018年 slowdony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
