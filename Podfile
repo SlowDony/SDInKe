07c54bb86054fec52ed0ea19233223ff5f1da081
slowdony <435574793@qq.com>
2018-01-05 00:06:09 +0800

    添加第三方

---
+++ Podfile
@@ -0,0 +1,8 @@
+target 'SDInKe' do
+pod 'SDWebImage'
+pod 'MJRefresh'
+pod 'MJExtension'
+pod 'YYKit'
+pod 'LFLiveKit'
+pod 'AFNetworking'
+end
---
+++ Pods/Target Support Files/Pods-SDInKe/Pods-SDInKe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SDInKe : NSObject
+@end
+@implementation PodsDummy_Pods_SDInKe
+@end
