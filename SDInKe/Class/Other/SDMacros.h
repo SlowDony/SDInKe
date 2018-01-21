68275cb968d50ec89a797696c5e5357ae93ae4e8
slowdony <435574793@qq.com>
2018-01-22 00:46:29 +0800

    添加

---
+++ SDInKe/Class/Other/SDMacros.h
@@ -0,0 +1,16 @@
+//
+//  SDMacros.h
+//  SDInKe
+//
+//  Created by slowdony on 2018/1/10.
+//  Copyright © 2018年 slowdony. All rights reserved.
+//
+
+#ifndef SDMacros_h
+#define SDMacros_h
+#define SCREEN_WIDTH    [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT    [UIScreen mainScreen].bounds.size.height
+
+
+
+#endif /* SDMacros_h */
