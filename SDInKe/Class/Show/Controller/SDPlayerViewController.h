a507304a22550972688ba62b06fbf549dd2e3263
slowdony <435574793@qq.com>
2018-05-11 00:18:10 +0800

    添加视频直播

---
+++ SDInKe/Class/Show/Controller/SDPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDPlayerViewController.h
+//  SDInKe
+//
+//  Created by slowdony on 2018/5/2.
+//  Copyright © 2018年 slowdony. All rights reserved.
+//
+
+#import "SDBaseViewController.h"
+
+@interface SDPlayerViewController : SDBaseViewController
+
+@end
